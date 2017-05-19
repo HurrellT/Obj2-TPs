93a6679d7b0596708f4b4e5ef8826aa23357f13c
mega_ <mega_@192.168.0.25>
2017-05-19 00:05:17 -0300

    1

--- TP5 MercadoCentral/MercadoCentral/TP5-Obj2/src/tp5_obj2_MercadoCentral/Caja.java
+++ TP5 MercadoCentral/MercadoCentral/TP5-Obj2/src/tp5_obj2_MercadoCentral/Caja.java
@@ -2,4 +2,10 @@ package tp5_obj2_MercadoCentral;
 
 public class Caja {
 
+	private int montoTotal;
+	
+	public void registrarProd(Producto prod) {
+		
+	}
+	
 }
---
+++ TP5 MercadoCentral/MercadoCentral/TP5-Obj2/src/tp5_obj2_MercadoCentral/MercadoTest.java
@@ -0,0 +1,14 @@
+package tp5_obj2_MercadoCentral;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MercadoTest extends Caja {
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
