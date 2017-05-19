a282f5bb90f5e9734d3e2d5450828da426983bad
tomas <tomas@tlh-note>
2017-05-19 15:26:41 -0300

    Cambios en proveedores y caja

--- TP5 MercadoCentral/MercadoCentral/TP5-Obj2/src/tp5_obj2_MercadoCentral/Caja.java
+++ TP5 MercadoCentral/MercadoCentral/TP5-Obj2/src/tp5_obj2_MercadoCentral/Caja.java
@@ -16,10 +16,10 @@ public class Caja {
 		this.montoTotal = montoTotal + nuevoMonto;
 	}
 	
-	public void registrarProd(Producto prod) {
-		this.updateMonto((prod.getCosto()));
+	public void registrarProd(Producto prod, Proveedor prov) {
+		this.updateMonto((prov.getProd(prod).getCosto()));
 		this.addProd(prod);
-//		prod.reduceStock();		reduce el stock al proveedor
+		prov.reduceStock();		//reduce el stock al proveedor
 	}
 
 	private void addProd(Producto prod) {
--- TP5 MercadoCentral/MercadoCentral/TP5-Obj2/src/tp5_obj2_MercadoCentral/Proveedor.java
+++ TP5 MercadoCentral/MercadoCentral/TP5-Obj2/src/tp5_obj2_MercadoCentral/Proveedor.java
@@ -1,5 +1,12 @@
 package tp5_obj2_MercadoCentral;
 
+import java.util.ArrayList;
+
 public interface Proveedor {
+
+	ArrayList<Producto> productos;
+//	private x stock;
+	
+	Producto getProd(Producto prod);
 
 }
